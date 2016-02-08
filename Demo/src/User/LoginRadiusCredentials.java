a97d6f4f9e6798e9839d84304b8d5fdffe2e99fa
Ashrujit Bhattacharya <ashru_bh@Ashrujits-MacBook-Air.local>
2016-02-08 18:36:05 +0530

    This is v1 demo

--- Demo/src/User/LoginRadiusCredentials.java
+++
@@ -1,7 +0,0 @@
-package User;
-
-public class LoginRadiusCredentials {
-	public static String apiKey= "78ebd621-71ac-4ab8-a057-3cd230d68cf8";
-	public static String apiSecret= "1757a66f-9bce-49f5-859a-00fed0c2a4da";
-
-}
